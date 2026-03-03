1576eebf7d96ada155fa5532b38dda1c99f1a474
Fernanda <fernandafcbpereira@gmail.com>
2026-03-03 10:37:20 -0300

    Alterando resolução da RI1

--- RI1.js
+++ RI1.js
@@ -33,80 +33,114 @@ const personagem2 =  {
       + "Nível de força: " + this.forca + "\n"
       + "Nível de velocidade: " + this.velocidade +"\n"
       + "Nível de resistência: " + this.resistencia
-      + "diferencial:" + this.diferencial
     }
 }
 
-const herois = [personagem1,personagem2]
+const personagem3 =  {
+    nome: "Tony Stark",
+    codinome: "Homem de Ferro",
+    armaPrincipal: "Armadura Mark 85",
+    armaSecundaria: "Lasers",
+    velocidade: 85,
+    forca: 75,
+    resistencia: 70,
+    descricao:function() {
+      return "Nome do personagem: " + this.nome + "\n"
+      + "Codinome do personagem: " + this.codinome + "\n"
+      + "Arma principal: " + this.armaPrincipal + "\n"
+      + "Arma secundaria: " + this.armaSecundaria + "\n"
+      + "Nível de força: " + this.forca + "\n"
+      + "Nível de velocidade: " + this.velocidade +"\n"
+      + "Nível de resistência: " + this.resistencia
+    }
+}
+
+
+
+
+const herois = [personagem1,personagem2,personagem3]
 
 
 console.log("--- INICIANDO TORNEIO DE COMPARAÇÕES =) ---\n");
 
 
 //força 
-let maisForte = herois[0]
-for(i=1; i< herois.length; i++){
+
+for(i=0; i< herois.length; i++){
+  for (let j = i + 1; j < herois.length; j++){
+    let lutador1 = herois[i];
+    let lutador2 = herois[j]; 
+
+   console.log(`\n=== COMPARANDO FORÇA: ${lutador1.codinome} VS ${lutador2.codinome} ===\n`);
    
-  // personagem1 vs personagem2 (descrição)
-   console.log(`\n=== COMPARANDO FORÇA: ${personagem1.codinome} VS ${personagem2.codinome} ===\n`);
-   
-   console.log(personagem1.descricao());
+   console.log(lutador1.descricao());
    console.log("\nVs.\n");
-   console.log(personagem2.descricao());
+   console.log(lutador2.descricao());
    console.log("-----------------------------------------");
 
-   if (maisForte.forca < herois[i].forca){
-      console.log(`${personagem2.codinome} VENCE em força!`);
+   if (lutador1.forca < lutador2.forca){
+      console.log(`${lutador2.codinome} VENCE em força!`);
       
-   }else{
-      console.log(`${personagem1.codinome} VENCE em força!`);
+   }else if(lutador2.forca > lutador1.forca){
+      console.log(`${lutador1.codinome} VENCE em força!`);
+   } else{
+      console.log("Empate")
    }
+ }
 }
 
-
-
-
 //Velocidade
-let maisVeloz = herois[0]
-for(i=1; i< herois.length; i++){
+
+for(i=0; i< herois.length; i++){
+   for (let j = i + 1; j < herois.length; j++){
+      let lutador1 = herois[i];
+      let lutador2 = herois[j]; 
    
-  // personagem1 vs personagem2 (descrição)
-   console.log(`\n=== COMPARANDO VELOCIDADE: ${personagem1.codinome} VS ${personagem2.codinome} ===\n`);
+   console.log(`\n=== COMPARANDO VELOCIDADE: ${lutador1.codinome} VS ${lutador2.codinome} ===\n`);
    
-   console.log(personagem1.descricao());
+   console.log(lutador1.descricao());
    console.log("\nVs.\n");
-   console.log(personagem2.descricao());
+   console.log(lutador2.descricao());
    console.log("-----------------------------------------");
 
-   if (maisVeloz.velocidade < herois[i].velocidade){
-      console.log(`${personagem2.codinome} VENCE em velocidade!`);
+   if (lutador1.velocidade < lutador2.velocidade){
+      console.log(`${lutador2.codinome} VENCE em velocidade!`);
       
-   }else{
-      console.log(`${personagem1.codinome} VENCE em velocidade!`);
+   }else if (lutador2.velocidade < lutador1.velocidade){
+      console.log(`${lutador1.codinome} VENCE em velocidade!`);
+   }else {
+     console.log("Empate")
    }
+ }
 }
 
 
 
 
 // resistencia
-let maisResistente = herois[0]
-for(i=1; i< herois.length; i++){
+
+for(i=0; i< herois.length; i++){
+  for (let j = i + 1; j < herois.length; j++){
+      let lutador1 = herois[i];
+      let lutador2 = herois[j]; 
    
-  // personagem1 vs personagem2 (descrição)
-   console.log(`\n=== COMPARANDO Resistencia: ${personagem1.codinome} VS ${personagem2.codinome} ===\n`);
+
+   console.log(`\n=== COMPARANDO Resistencia: ${lutador1.codinome} VS ${lutador2.codinome} ===\n`);
    
-   console.log(personagem1.descricao());
+   console.log(lutador1.descricao());
    console.log("\nVs.\n");
-   console.log(personagem2.descricao());
+   console.log(lutador2.descricao());
    console.log("-----------------------------------------");
 
-   if (maisResistente.resistencia < herois[i].resistencia){
-      console.log(`${personagem2.codinome} VENCE em resistente!`);
+   if (lutador1.resistencia < lutador2.resistencia){
+      console.log(`${lutador2.codinome} VENCE em resistente!`);
       
+   }else if (lutador2.resistencia < lutador1.resistencia){
+      console.log(`${lutador1.codinome} VENCE em resistente!`);
    }else{
-      console.log(`${personagem1.codinome} VENCE em resistente!`);
+      console.log("Empate")
    }
+ }
 }
 
 
